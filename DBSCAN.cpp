a9b4331d658b284f28cb6936f73f939735c4c37d
wrld <974186763@qq.com>
2020-07-10 16:58:56 +0800

    0710 16:58

--- DBSCAN.cpp
+++ DBSCAN.cpp
@@ -1,4 +1,14 @@
 #include "DBSCAN.hpp"
+
+void viewerOneOff(pcl::visualization::PCLVisualizer &viewer, double x, double y,
+                  double z, string name) {
+  pcl::PointXYZ o;
+  o.x = x;
+  o.y = y;
+  o.z = z;
+  viewer.addSphere(o, 0.001, 255, 0, 0, name, 0);
+}
+
 void showCloud(pcl::PointCloud<pcl::PointXYZRGB>::Ptr cloud1,
                pcl::PointCloud<pcl::PointXYZ>::Ptr cloud2) {
   boost::shared_ptr<pcl::visualization::PCLVisualizer> viewer(
@@ -21,6 +31,7 @@ void showCloud(pcl::PointCloud<pcl::PointXYZRGB>::Ptr cloud1,
   };
 }
 void DBSCAN::start_scan() {
+  start = clock();
   select_kernel();
   find_independent();
 }
@@ -81,7 +92,13 @@ vector<int> DBSCAN::vectors_set_union(vector<int> v1, vector<int> v2) {
   set_union(v1.begin(), v1.end(), v2.begin(), v2.end(), back_inserter(v));
   return v;
 }
-
+vector<int> DBSCAN::vectors_set_diff(vector<int> v1, vector<int> v2) {
+  vector<int> v;
+  sort(v1.begin(), v1.end());
+  sort(v2.begin(), v2.end());
+  set_difference(v1.begin(), v1.end(), v2.begin(), v2.end(), back_inserter(v));
+  return v;
+}
 void DBSCAN::find_independent() {
   cout << "start find intersection of cluster and union them" << endl;
   for (int i = 0; i < core_points.size(); i++) {
@@ -91,11 +108,21 @@ void DBSCAN::find_independent() {
       vector<int> result;
       result = vectors_intersection(neighbourPoints[core_points[i]],
                                     neighbourPoints[core_points[j]]);
-      if (result.size() > 0 && neighbourPoints[core_points[i]].size() < 600 &&
-          neighbourPoints[core_points[j]].size() < 600) {
-        neighbourPoints[core_points[i]] = vectors_set_union(
-            neighbourPoints[core_points[i]], neighbourPoints[core_points[j]]);
-        neighbourPoints[core_points[j]].clear();
+      if (result.size() > 20) {
+        if (neighbourPoints[core_points[i]].size() < 500 &&
+            neighbourPoints[core_points[j]].size() < 500 &&
+            (neighbourPoints[core_points[i]].size() +
+             neighbourPoints[core_points[j]].size()) < 600) {
+          neighbourPoints[core_points[i]] = vectors_set_union(
+              neighbourPoints[core_points[i]], neighbourPoints[core_points[j]]);
+          neighbourPoints[core_points[j]].clear();
+        }
+        // else {
+        //   neighbourPoints[core_points[j]] = vectors_set_diff(
+        //       neighbourPoints[core_points[i]],
+        //       neighbourPoints[core_points[j]]);
+        //   // neighbourPoints[core_points[j]].clear();
+        // }
       }
     }
   }
@@ -121,6 +148,8 @@ void DBSCAN::find_independent() {
 
   pcl::PointCloud<pcl::PointXYZRGB>::Ptr result_cloud(
       new pcl::PointCloud<pcl::PointXYZRGB>);
+  this->cluster_center =
+      pcl::PointCloud<pcl::PointXYZ>::Ptr(new pcl::PointCloud<pcl::PointXYZ>);
   for (auto i = 0; i < core_points.size(); i++) {
     if (neighbourPoints[core_points[i]].size() == 0 ||
         neighbourPoints[core_points[i]].size() < 200)
@@ -134,6 +163,7 @@ void DBSCAN::find_independent() {
     int B = rand() % 255;
     pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_cluster(
         new pcl::PointCloud<pcl::PointXYZ>);
+
     for (auto j = 0; j < neighbourPoints[core_points[i]].size(); j++) {
       pcl::PointXYZRGB point;
       point.r = R;
@@ -149,19 +179,120 @@ void DBSCAN::find_independent() {
     cloud_cluster->width = cloud_cluster->points.size();
     cloud_cluster->height = 1;
     cloud_cluster->is_dense = true;
+    sum_points += cloud_cluster->points.size();
+    points_num.push_back(cloud_cluster->points.size());
+    Eigen::Vector4f centroid;
+    pcl::compute3DCentroid(*cloud_cluster, centroid);
+    // std::cout << "The XYZ coordinates of the centroid are: (" <<
+    // centroid[0]
+    //           << ", " << centroid[1] << ", " << centroid[2] << ")."
+    //           << std::endl;
+    cluster_center->points.push_back(
+        pcl::PointXYZ(centroid[0], centroid[1], centroid[2]));
+    cluster_centroid.push_back(centroid);
     this->result_cloud_.push_back(cloud_cluster);
   }
-
+  cluster_center->width = cluster_center->points.size();
+  cluster_center->height = 1;
+  cluster_center->is_dense = true;
   cout << "the sum of clusters: " << this->result_cloud_.size() << endl;
   result_cloud->width = result_cloud->points.size();
   result_cloud->height = 1;
   result_cloud->is_dense = true;
-  // pcl::visualization::PCLVisualizer viewer("example");
-  // viewer.addPointCloud<pcl::PointXYZRGB>(result_cloud, "result");
-  // while (!viewer.wasStopped()) {
-  //   viewer.spinOnce();
+
+  end = clock();
+  double endtime = (double)(end - start) / CLOCKS_PER_SEC;
+  cout << "Total time:" << end << "  " << start << "  " << endtime << "s"
+       << endl;  // s为单位
+  vector<pcl::PointXYZ> cluster_vectors;
+  vector<pcl::PointXYZ> centroidXYZ_value;
+  if (cluster_center->points.size() > 1) {
+    pcl::KdTreeFLANN<pcl::PointXYZ> kdtree;
+    kdtree.setInputCloud(cluster_center);
+
+    for (int i = 0; i < cluster_center->points.size(); i++) {
+      cluster temp;
+      temp.index = i;
+      cluster_score.push_back(temp);
+      vector<int> indices;
+      vector<float> dists;
+      kdtree.nearestKSearch(cluster_center->points[i],
+                            int(cluster_center->points.size() / 2) + 1, indices,
+                            dists);
+      double sum = 0;
+      for (auto dist : dists) {
+        sum = sum + dist;
+      }
+      cluster_score[i].dense = sum;
+      // pca(result_cloud_[i]);
+      // Eigen::Vector4f centroid;
+      // Eigen::Matrix3f covariance_matrix;
+
+      // // Extract the eigenvalues and eigenvectors
+      // Eigen::Vector3f eigen_values;
+      // Eigen::Matrix3f eigen_vectors;
+
+      // // pcl::compute3DCentroid(*result_cloud_[i], centroid);
+
+      // // Compute the 3x3 covariance matrix
+      // pcl::computeCovarianceMatrix(*result_cloud_[i], cluster_centroid[i],
+      //                              covariance_matrix);
+      // pcl::eigen33(covariance_matrix, eigen_vectors, eigen_values);
+      // PointXYZ centroidXYZ;
+      // centroidXYZ.getVector4fMap() = cluster_centroid[i];
+      // centroidXYZ_value.push_back(centroidXYZ);
+      // PointXYZ Point1 =
+      //     PointXYZ((cluster_centroid[i](0) + eigen_vectors.col(0)(0)),
+      //              (cluster_centroid[i](1) + eigen_vectors.col(0)(1)),
+      //              (cluster_centroid[i](2) + eigen_vectors.col(0)(2)));
+      // cluster_vectors.push_back(Point1);
+
+      // _viewer->addLine<pcl::PointXYZRGB> (centroid, eigen_vectors, "line");
+    }
+    // sort(cluster_score.begin(), cluster_score.end(), cmp);
+  } else {
+    cluster temp;
+    temp.index = 0;
+    cluster_score.push_back(temp);
+  }
+  // for (int i = 0; i < cluster_score.size(); i++) {
+  //   cluster_score[i].dense_index = i + 1;
+  //   // cout << cluster_score[i].dense_index << "  " <<
+  //   cluster_score[i].dense
+  //   //      << " " << cluster_score[i].index << endl;
   // }
-  showCloud(result_cloud, origin_cloud_);
+  if (view_on) {
+    boost::shared_ptr<pcl::visualization::PCLVisualizer> viewer(
+        new pcl::visualization::PCLVisualizer("3D Viewer"));
+    int v1(0);
+    viewer->createViewPort(0.0, 0.0, 0.5, 1.0, v1);
+    viewer->setBackgroundColor(0, 0, 0, v1);
+    viewer->addPointCloud<pcl::PointXYZRGB>(result_cloud, "sample cloud1", v1);
+
+    int v2(0);
+    viewer->createViewPort(0.5, 0.0, 1.0, 1.0, v2);
+    viewer->addPointCloud<pcl::PointXYZ>(origin_cloud_, "sample cloud2", v2);
+    viewer->setBackgroundColor(0.3, 0.3, 0.3, v2);
+    // viewer->addCoordinateSystem(1.0);
+
+    viewer->initCameraParameters();
+    for (int i = 0; i < cluster_centroid.size(); i++) {
+      std::stringstream ss;
+      ss << "cloud_cluster_" << i;
+      viewerOneOff(*viewer, cluster_centroid[i][0], cluster_centroid[i][1],
+                   cluster_centroid[i][2], ss.str());
+      showCloud(result_cloud, result_cloud_[i]);
+      // std::stringstream ss2;
+      // ss2 << "arrow" << i;
+      // viewer->addArrow(cluster_vectors.at(i), centroidXYZ_value.at(i), 0.5,
+      // 0.5,
+      //                  0.5, false, ss2.str());
+    }
+    while (!viewer->wasStopped()) {
+      viewer->spinOnce(100);
+      // boost::this_thread::sleep(boost::posix_time::microseconds(100000));
+    };
+  }
 }
 int main() {
   pcl::PointCloud<pcl::PointXYZ>::Ptr cloud(new pcl::PointCloud<pcl::PointXYZ>);
@@ -169,6 +300,30 @@ int main() {
   pcl::io::loadPCDFile(
       "/home/gjx/orbslam/catkin_ws/src/ZJUBinPicking/pcd_files/filter_1.pcd",
       *cloud);
-  DBSCAN gather(cloud, 0.010f, 40);  // 0.011
+  double dense = 0.01;
+  double result_dense;
+  double min = 10000000;
+  // cout << dense << endl;
+  // for (int i = 0; i < 5; i++) {
+  //   DBSCAN gather(cloud, dense, 40);  // 0.011
+  //   gather.view_on = 0;
+  //   gather.start_scan();
+
+  //   double avr = gather.sum_points / gather.points_num.size();
+  //   double var = 0;
+  //   for (int i = 0; i < gather.points_num.size(); i++) {
+  //     var += (gather.points_num[i] - avr) * (gather.points_num[i] - avr);
+  //   }
+  //   var = var / gather.points_num.size();
+  //   cout << "var" << var << endl;
+
+  //   if (var < min) {
+  //     min = var;
+  //     result_dense = dense;
+  //   }
+  //   dense -= 0.0001;
+  // }
+  cout << "result dense" << result_dense << endl;
+  DBSCAN gather(cloud, 0.01, 30);  // 0.011
   gather.start_scan();
 }
--- DBSCAN.hpp
+++ DBSCAN.hpp
@@ -1,16 +1,40 @@
+#include <pcl/common/centroid.h>
+#include <pcl/common/transforms.h>
+#include <pcl/features/integral_image_normal.h>  //法线估计类头文件
+#include <pcl/features/normal_3d.h>
+#include <pcl/io/io.h>
 #include <pcl/io/pcd_io.h>
 #include <pcl/kdtree/kdtree_flann.h>
 #include <pcl/octree/octree.h>
 #include <pcl/point_cloud.h>
 #include <pcl/point_types.h>
+#include <pcl/segmentation/extract_clusters.h>
+#include <pcl/segmentation/min_cut_segmentation.h>
+#include <pcl/segmentation/region_growing.h>
+#include <pcl/segmentation/sac_segmentation.h>
 #include <pcl/visualization/cloud_viewer.h>
 #include <pcl/visualization/pcl_visualizer.h>
 
+#include <ctime>
 #include <iostream>
+#include <numeric>
+#include <opencv2/core/core.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
 #include <vector>
+
 using namespace pcl;
 using namespace std;
-
+using namespace cv;
+struct cluster {
+  int index;
+  int dense_index;
+  double dense;
+  int height_index;
+  double height;
+  int pose_index;
+  double pose;
+  double score;
+};
 class DBSCAN {
  public:
   DBSCAN(pcl::PointCloud<pcl::PointXYZ>::Ptr cloud, double eps, double MinPts)
@@ -27,27 +51,36 @@ class DBSCAN {
   }
   double MinPts;
   double eps;
-  double MinbPts = 20;
+  double MinbPts = 10;
+  clock_t start, end;
   ~DBSCAN() {}
   pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_;
   pcl::PointCloud<pcl::PointXYZ>::Ptr origin_cloud_;
   vector<pcl::PointCloud<pcl::PointXYZ>::Ptr> result_cloud_;
+  pcl::PointCloud<pcl::PointXYZ>::Ptr cluster_center;
   vector<int> cluster_type;
   vector<int> core_points;
   vector<int> bound_points;
   vector<int> result_points;
-  int method_ = 1;
+  bool view_on = 1;
+  vector<Eigen::Vector4f> cluster_centroid;
+  int method_ = 0;
   int use_edge = 1;
+  int sum_points = 0;
+  vector<int> points_num;
   const int CORE_POINT = 0;
   const int BOUND_POINT = 1;
   const int NOISE_POINT = 2;
   const int KD_TREE = 0;
   const int OCT_TREE = 1;
+  vector<cluster> cluster_score;
   std::vector<std::vector<int>> neighbourPoints;
   std::vector<std::vector<float>> neighbourDistance;
   void start_scan();
   void select_kernel();
   void find_independent();
+  vector<int> vectors_set_diff(vector<int> v1, vector<int> v2);
   vector<int> vectors_intersection(vector<int> v1, vector<int> v2);
   vector<int> vectors_set_union(vector<int> v1, vector<int> v2);
+  void normal(pcl::PointCloud<pcl::PointXYZ>::Ptr cloud);
 };
